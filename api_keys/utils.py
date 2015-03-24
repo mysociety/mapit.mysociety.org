534e9e944bf9f24b41c1f7c14b75a832bff0b181
Steven Day <steve@mysociety.org>
2015-03-24 15:40:53 +0000

    Make redis key strings constants

--- api_keys/utils.py
+++ api_keys/utils.py
@@ -5,6 +5,7 @@ from django.conf import settings
 
 _connection = None
 
+
 def redis_connection():
     global _connection
     if _connection is None:
@@ -16,3 +17,14 @@ def redis_connection():
         )
     return _connection
 
+
+class RedisStrings(object):
+    API_RESTRICT = 'api:{0}:restricted'.format(settings.REDIS_API_NAME)
+    API_THROTTLE = 'api:{0}:throttled'.format(settings.REDIS_API_NAME)
+    API_THROTTLE_COUNTER = 'api:{0}:counter'.format(settings.REDIS_API_NAME)
+    API_THROTTLE_BLOCKED = 'api:{0}:blocked'.format(settings.REDIS_API_NAME)
+    API_THROTTLE_DEFAULT_LIMIT = 'api:{0}:default_max'.format(settings.REDIS_API_NAME)
+
+    @staticmethod
+    def rate_limit_string(identity):
+        return "key:{0}:usage:{1}:max".format(identity, settings.REDIS_API_NAME)
