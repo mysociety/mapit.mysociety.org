aece54596d3dc423d52b1f764cf0336256c6fb8d
Steven Day <steve@mysociety.org>
2015-03-24 09:54:32 +0000

    Only cache templates when debug is false
    This makes development of templates much easier

--- mapit-gb/settings.py
+++ mapit-gb/settings.py
@@ -145,11 +145,13 @@ STATICFILES_FINDERS = (
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
-    ('django.template.loaders.cached.Loader', (
-        'django.template.loaders.filesystem.Loader',
-        'django.template.loaders.app_directories.Loader',
-    )),
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
 )
+if not DEBUG:
+    TEMPLATE_LOADERS = (
+        ('django.template.loaders.cached.Loader', TEMPLATE_LOADERS),
+    )
 
 # UpdateCacheMiddleware does ETag setting, and
 # ConditionalGetMiddleware does ETag checking.
