befcc186f0d297f94e824b0a9555242abc26f0b4
Steven Day <steve@mysociety.org>
2015-04-15 10:59:44 +0100

    FIXUP: Change verbosity of command output

--- api_keys/management/commands/api_keys_restrict_api.py
+++ api_keys/management/commands/api_keys_restrict_api.py
@@ -6,6 +6,7 @@ from django.conf import settings
 
 from ...utils import redis_connection, RedisStrings
 
+
 class Command(NoArgsCommand):
     help = """Restrict access the api named in settings.REDIS_API_NAME if
              settings.API_RESTRICT is True, un-restrict it otherwise."""
@@ -14,9 +15,9 @@ class Command(NoArgsCommand):
         r = redis_connection()
         if settings.API_RESTRICT:
             r.set(RedisStrings.API_RESTRICT, '1')
-            if options['verbosity'] > 2:
+            if options['verbosity'] > 1:
                 self.stdout.write("Restricted api: {0}".format(settings.REDIS_API_NAME))
         else:
             r.delete(RedisStrings.API_RESTRICT)
-            if options['verbosity'] > 2:
+            if options['verbosity'] > 1:
                 self.stdout.write("Removed restriction on api: {0}".format(settings.REDIS_API_NAME))
