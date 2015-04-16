fe2c95b51c0fef502409b4b1d54e2e6fef20f93b
Steven Day <steve@mysociety.org>
2015-04-16 12:27:27 +0100

    Rename the Django project to mapit_mysociety_org

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mapit-gb.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mapit_mysociety_org.settings")
 
     from django.core.management import execute_from_command_line
 
