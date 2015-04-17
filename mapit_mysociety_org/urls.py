bd228bbe7c3e971c093ad3c1fb5111a74dbb6cc2
Steven Day <steve@mysociety.org>
2015-04-17 10:08:03 +0100

    Turn off the account settings page from django user accounts

--- mapit_mysociety_org/urls.py
+++ mapit_mysociety_org/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.defaults import page_not_found
 admin.autodiscover()
 
 urlpatterns = [
     url(r'^', include('mapit.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r"^account/api_keys/", include("api_keys.urls")),
+    # Turn off the settings url from the account app, as we don't want it.
+    url(r"^account/settings/", page_not_found),
     url(r"^account/", include("account.urls")),
 ]
