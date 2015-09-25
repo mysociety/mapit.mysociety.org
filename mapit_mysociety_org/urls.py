d15b7b15c83c81b07caeb2c1fce48587d4b5ff9b
Steven Day <steve@mysociety.org>
2015-09-25 10:46:50 +0100

    Make user accounts only require an email address
    This overrides the login and signup views from django-user-accounts
    so that they use forms which don't need a username, setting it
    automatically to the user's email address instead

--- mapit_mysociety_org/urls.py
+++ mapit_mysociety_org/urls.py
@@ -3,11 +3,17 @@ from django.contrib import admin
 from django.views.defaults import page_not_found
 admin.autodiscover()
 
+from .views import LoginView, SignupView
+
 urlpatterns = [
     url(r'^', include('mapit.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r"^account/api_keys/", include("api_keys.urls")),
     # Turn off the settings url from the account app, as we don't want it.
     url(r"^account/settings/", page_not_found),
+    # Override the login and signup views from the account app, so we can use
+    # our versions which use an email address instead of a username.
+    url(r"^account/signup/$", SignupView.as_view(), name="account_signup"),
+    url(r"^account/login/$", LoginView.as_view(), name="account_login"),
     url(r"^account/", include("account.urls")),
 ]
---
+++ mapit_mysociety_org/views.py
@@ -0,0 +1,22 @@
+import account.forms
+import account.views
+
+from . import forms
+
+
+class LoginView(account.views.LoginView):
+    """ Override account.views.LoginView to use the email-only version """
+
+    form_class = account.forms.LoginEmailForm
+
+
+class SignupView(account.views.SignupView):
+    """ Override account.views.SignupView to use our email-only SignupForm """
+
+    form_class = forms.SignupForm
+
+    def generate_username(self, form):
+        # Return the email address as the username (Django's user model needs
+        # something to store there).
+        print "generating username: {0}".format(form.cleaned_data['email'])
+        return form.cleaned_data['email']
