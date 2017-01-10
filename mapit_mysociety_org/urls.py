de6b5604610a052dfc3298e2ac0d16e8d19682ec
Zarino Zappia <mail@zarino.co.uk>
2017-01-10 16:01:30 +0000

    Tidy up account pages

--- mapit_mysociety_org/urls.py
+++ mapit_mysociety_org/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from django.views.defaults import page_not_found
 
 from django.shortcuts import render
 
@@ -13,8 +12,6 @@ urlpatterns = [
     url(r'^docs/', render, {'template_name': 'docs.html'}, 'mapit_docs'),
     url(r'^admin/', include(admin.site.urls)),
     url(r"^account/api_keys/", include("api_keys.urls")),
-    # Turn off the settings url from the account app, as we don't want it.
-    url(r"^account/settings/", page_not_found),
     # Override the login and signup views from the account app, so we can use
     # our versions which use an email address instead of a username.
     url(r"^account/signup/$", SignupView.as_view(), name="account_signup"),
