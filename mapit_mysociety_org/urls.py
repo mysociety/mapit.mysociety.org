7b2d411f565ddc808159494fb1b1e8cb7cd92c4d
Zarino Zappia <mail@zarino.co.uk>
2017-01-13 11:34:01 +0000

    Move copyright info from footer to legal page

--- mapit_mysociety_org/urls.py
+++ mapit_mysociety_org/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     url(r'^changelog$', render, {'template_name': 'changelog.html'}, 'mapit_changelog'),
     url(r'^', include('mapit.urls')),
     url(r'^pricing/', render, {'template_name': 'pricing.html'}, 'mapit_pricing'),
+    url(r'^legal/', render, {'template_name': 'mapit/licensing.html'}, 'mapit_legal'),
     url(r'^docs/', render, {'template_name': 'docs.html'}, 'mapit_docs'),
     url(r'^admin/', include(admin.site.urls)),
     url(r"^account/api_keys/", include("api_keys.urls")),
