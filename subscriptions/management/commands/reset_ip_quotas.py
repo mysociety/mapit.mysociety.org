b0abfd8ec4eaaeae3874baf3d11857fa08df51ac
Matthew Somerville <matthew-github@dracos.co.uk>
2017-03-14 12:55:47 +0000

    Reset all IP quotas daily, not just blocked ones.

--- subscriptions/management/commands/reset_ip_quotas.py
+++ subscriptions/management/commands/reset_ip_quotas.py
@@ -4,21 +4,14 @@ from django.conf import settings
 from api_keys.utils import redis_connection
 
 
-def redis_set(r, key):
-    val = r.spop(key)
-    while val:
-        yield val
-        val = r.spop(key)
-
-
 class Command(BaseCommand):
     help = """Reset IP address daily usage blocks."""
 
     def handle(self, **options):
         r = redis_connection()
-        key = 'api:%s:blocked_ips' % settings.REDIS_API_NAME
-        for ip in redis_set(r, key):
-            r.delete('user:%s:quota:%s:blocked' % (ip, settings.REDIS_API_NAME))
-            r.delete('user:%s:quota:%s:count' % (ip, settings.REDIS_API_NAME))
+        for key in r.scan_iter(match='user:*.*:quota:%s:count' % settings.REDIS_API_NAME, count=100):
+            r.delete(key)
             if options['verbosity'] > 1:
-                self.stdout.write("Removed IP %s" % ip)
+                self.stdout.write("Removed IP %s" % key)
+        for key in r.scan_iter(match='user:*.*:quota:%s:blocked' % settings.REDIS_API_NAME, count=100):
+            r.delete(key)
