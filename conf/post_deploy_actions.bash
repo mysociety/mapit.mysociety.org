3cb06045ee2cc16bdd74a5282d0073ebfbe44dc9
Matthew Somerville <matthew-github@dracos.co.uk>
2017-03-16 10:30:23 +0000

    Make sure database exists before  site creation.
    On a first deploy, it won't yet exist at this point.

--- conf/post_deploy_actions.bash
+++ conf/post_deploy_actions.bash
@@ -10,3 +10,7 @@ source .venv/bin/activate
 
 # get the database up to speed
 python manage.py migrate
+
+# Configure the default site's base url, so that link-building using the sites
+# framework works.
+python manage.py create_default_site
--- conf/pre_deploy_actions.bash
+++ conf/pre_deploy_actions.bash
@@ -42,10 +42,6 @@ bin/mapit_make_css
 # gather all the static files in one place
 python manage.py collectstatic --noinput --link
 
-# Configure the default site's base url, so that link-building using the sites
-# framework works.
-python manage.py create_default_site
-
 # Make sure the Redis api key checking/throttling is set up as-per the config
 # settings
 python manage.py api_keys_restrict_api
