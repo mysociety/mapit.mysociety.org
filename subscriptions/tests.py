ae5cec0db250c7b3baaf7c22a530e8555e3804e9
Sam Pearson <sam@sgp.me.uk>
2022-05-23 11:55:21 +0100

    Actually test that the reset_ip_quotas script is working
    This commit adds a check before and after the reset_ip_quotas management
    command is called in the tests to ensure that the expected values are
    present before and absent after.

--- subscriptions/tests.py
+++ subscriptions/tests.py
@@ -569,7 +569,14 @@ class ManagementTest(PatchedRedisTestCase):
         r = redis_connection()
         r.set('user:127.0.0.2:quota:test_api:count', 42)
         r.set('user:127.0.0.3:quota:test_api:blocked', 1)
+
+        self.assertEqual(r.get('user:127.0.0.2:quota:test_api:count'), b'42')
+        self.assertEqual(r.get('user:127.0.0.3:quota:test_api:blocked'), b'1')
+
         call_command('reset_ip_quotas', stdout=StringIO(), stderr=StringIO(), **kwargs)
+
+        self.assertIsNone(r.get('user:127.0.0.2:quota:test_api:count'))
+        self.assertIsNone(r.get('user:127.0.0.3:quota:test_api:blocked'))
 
     def test_reset_ip_quotas(self):
         self._test_reset_ip_quotas()
